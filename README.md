2f0feb6be3c479230baab40a6ca7307d95025b2b
Fahimefto <ifto.141.fi@gmail.com>
2022-08-25 19:14:07 +0600

    Readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# MERN TODO APPS
+
+## a todo management web-app
+
+#### Stack
+
+- Next.js
+- Tailwind-css
+- Kometa UI
+- Express
+- MongoDB
+
+  <img src="./DocImg/Screenshot from 2022-08-25 18-59-47.png">
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,18 +4,18 @@ export const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div class="bg-gray-900">
-      <div class="px-4 py-6 mx-auto lg:py-8 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div class="relative flex items-center justify-between lg:justify-center lg:space-x-16">
-          <ul class="flex items-center hidden space-x-8 lg:flex"></ul>
+    <div className="bg-gray-900">
+      <div className="px-4 py-6 mx-auto lg:py-8 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between lg:justify-center lg:space-x-16">
+          <ul className="flex items-center hidden space-x-8 lg:flex"></ul>
           <a
             href="/"
             aria-label="TODO APP"
             title="Company"
-            class="inline-flex items-center"
+            className="inline-flex items-center"
           >
             <svg
-              class="w-8 text-teal-accent-400"
+              className="w-8 text-teal-accent-400"
               viewBox="0 0 24 24"
               color="white"
               strokeLinejoin="round"
@@ -30,19 +30,19 @@ export const Nav = () => {
               <rect x="14" y="1" width="7" height="6" />
               <rect x="14" y="11" width="7" height="12" />
             </svg>
-            <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+            <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
               TODO APP
             </span>
           </a>
 
-          <div class="lg:hidden">
+          <div className="lg:hidden">
             <button
               aria-label="Open Menu"
               title="Open Menu"
-              class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
               onClick={() => setIsMenuOpen(true)}
             >
-              <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
                   d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
